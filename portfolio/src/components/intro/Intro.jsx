655a390a092852f64c27bd023b3d8f650fe08985
Soul Lee <lee.soul12@gmail.com>
2022-02-28 22:22:14 -0500

    put pictures on the folder and display them

--- portfolio/src/components/intro/Intro.jsx
+++ portfolio/src/components/intro/Intro.jsx
@@ -3,6 +3,22 @@ import './Intro.scss'
 
 export default function Intro() {
   return (
-    <div className='intro' id='intro'>Intro</div>
+    <div className='intro' id='intro'>
+      <div className='left'>
+        <div className='imageContainer'>
+          <img src="assets/image.png" alt='image'/>
+        </div>
+      </div>
+      <div className='right'>
+        <div className='wrapper'>
+          <h2>Hi there I am</h2>
+          <h1>Soul Lee</h1>
+          <h3>Ahhhh
+            <span>
+            </span>
+          </h3>
+        </div>
+      </div>
+    </div>
   )
 }
